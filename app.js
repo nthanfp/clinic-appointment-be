7859ac8d5d78b80878dc84d3d1c857aa75c735e0
Nathanael Ferry P <ntnferry1505@gmail.com>
2021-07-30 19:11:37 +0700

    make it port better

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ import dbUtil from './utils/db.js';
 import JsonException from './utils/JsonException.js';
 
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 80;
 
 // cors options
 const whitelist = ['http://localhost:8000', 'http://localhost:8080', 'http://localhost:3000'];
